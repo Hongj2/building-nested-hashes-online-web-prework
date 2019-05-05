b7fc9cd889244992b81ee0dbe86c8d6d4b9404c4
Jessica Hong <jessicahong.24@gmail.com>
2019-05-05 21:25:29 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,17 +1,29 @@
 def second_challenge
   epic_tragedy = {
    :montague => {
+<<<<<<< HEAD
       :patriarch => {},
       :matriarch => {},
       :hero => {},
+=======
+      :patriarch => {name: "Lord Montague", age: "53"},
+      :matriarch => {name: "Lady Montague", age: "54"},
+      :hero => {name: "Romeo", age: "15", status: "alive"},
+>>>>>>> 7d03fca376a0bcd90e494b282dd88d9b8059c651
       :hero_friends => [
         #your hashes here!
       ]
    }, 
    :capulet => {
+<<<<<<< HEAD
       :patriarch => {},
       :matriarch => {},
       :heroine => {},
+=======
+      :patriarch => {name: "Lord Capulet", age: "50"},
+      :matriarch => {name: "Lady Capulet", age: "51"},
+      :heroine => {name: "Juliet", age: "15", status: "alive"},
+>>>>>>> 7d03fca376a0bcd90e494b282dd88d9b8059c651
       :heroine_friends => [
         #your hashes here!
       ]
